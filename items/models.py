798e0bf76eaa45f4b91067f70cbce974b30dfe6d
Juan Felipe Calixto Londoño <Juan.calixto@unp.gov.co>
2025-11-28 16:47:59 -0500

    COntinuation of the project with meta

--- items/models.py
+++ items/models.py
@@ -4,12 +4,18 @@ from django.db import models
 from django.db import models
 
 class PersonaPDP(models.Model):
-    NombrePDP = models.CharField(max_length=200)
-    CedulaPDP = models.TextField()
-    SerialPDP = models.CharField(max_length=50, default='New')
+    nombre_pdp = models.CharField(max_length=200)
+    cedula_pdp = models.TextField()
+    serial_pdp = models.CharField(max_length=50, default='New', db_column="serial_pdp")
     
-    # This is our bridge to SharePoint
+    # Bridge to SharePoint
     sharepoint_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.NombrePDP
+
+    class Meta:
+        #verbose_name = "PersonaPDP"
+        #verbose_name_plural = "PersonasPDP"
+        db_table = "persona_pdp"
+        managed = False
--- items/serializer.py
+++ items/serializer.py
@@ -4,4 +4,5 @@ from .models import PersonaPDP
 class PersonaPDP_Serializer(serializers.ModelSerializer):
     class Meta:
         model = PersonaPDP
-        fields = '__all__'
+        fields = ('nombre_pdp', 'cedula_pdp', 'serial_pdp', 'sharepoint_id')
+        read_only_fields = ('sharepoint_id',)
